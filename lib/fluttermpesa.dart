c12ff79064401cc81c72ac315b8346d3dcd8ca89
Derrick Mungai <derrickmungai@gmail.com>
2020-06-24 08:45:02 +0000

    Chore: Implemented API calls

--- lib/fluttermpesa.dart
+++ lib/fluttermpesa.dart
@@ -1,6 +1,7 @@
 library fluttermpesa;
 
 import 'dart:convert';
+import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:fluttermpesa/Request.dart';
 import 'package:fluttermpesa/Response.dart';
@@ -8,22 +9,26 @@ import 'AccessToken.dart';
 
 class Mpesa {
 
-  var productionUrl="https://api.safaricom.co.ke/";
-  var sandboxUrl="https://sandbox.safaricom.co.ke/";
+  var productionUrl="https://api.safaricom.co.ke";
+  var sandboxUrl="https://sandbox.safaricom.co.ke";
 
 
-  Future<AccessToken> accessToken() async{
-
-    final http.Response response = await http.get( sandboxUrl+"/oauth/v1/generate?grant_type=client_credentials");
-
+  Future<AccessToken> accessToken(String consumerKey, String consumerSecret) async{
+    var auth = 'Basic '+base64Encode(utf8.encode('$consumerKey:$consumerSecret'));
+    final http.Response response = await http.get( sandboxUrl+"/oauth/v1/generate?grant_type=client_credentials",
+        headers: {HttpHeaders.authorizationHeader: auth});
     return AccessToken.fromJson(jsonDecode(response.body));
 
   }
 
-  Future<Response> lipaNaMpesa(Request request) async{
+  Future<Response> lipaNaMpesa(Request request, String consumerKey, String consumerSecret) async{
+    AccessToken accessToken=this.accessToken(consumerKey, consumerSecret) as AccessToken;
+    String token=accessToken.accessToken;
+
     var json = jsonEncode(request.toJson());
     final http.Response response = await http.post( sandboxUrl+"/Mpesa/b2c/v1/paymentrequest",
-        headers: {"Content-Type": "application/json"},
+        headers: {"Content-Type": "application/json",
+        "Authorization":"Bearer"+ "$token"},
         body: json);
     return Response.fromJson(jsonDecode(response.body));
 
